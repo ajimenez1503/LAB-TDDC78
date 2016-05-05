7bfcd407e33c35ed5e025f2443f6fc3a1f8fad0c
softwarejimenez <softwarejimenezz@gmial.com>
2016-05-05 10:22:33 +0200

    send a receive buffer between processor with the virtual topology

--- lab5/main.c
+++ lab5/main.c
@@ -15,7 +15,7 @@ int main (int argc, char ** argv) {
     MPI_Comm com = MPI_COMM_WORLD;
     //printf("ID= %d of number of processors %d\n",rank,np );
 
-    //printf("ID= %d create topologies virtual %d\n",rank,np );
+    //printf("ID= %d create topologies virtual n",rank);
     int dims[2]; // 2D matrix / grid
     dims[0]= 3; // 2 rows
     dims[1]= 3; // 2 columns
@@ -27,6 +27,7 @@ int main (int argc, char ** argv) {
     MPI_Cart_create(MPI_COMM_WORLD,2,dims,periods,reorder,&grid_comm);
 
     /////////////Define type
+    //printf("ID= %d define type particle \n",rank );
     MPI_Datatype PART_MPI; // MPI type to commit
     int block_lengths [] = { 1, 1, 1, 1 }; // Lengths of type elements
     MPI_Datatype block_types [] = { MPI_FLOAT,MPI_FLOAT,MPI_FLOAT,MPI_FLOAT }; //Set types
@@ -82,7 +83,6 @@ int main (int argc, char ** argv) {
         }
 */
 
-    //printf("ID= %d finish initialize \n",rank );
     //printf("ID= %d Init check collision \n",rank );
     //create 4 buffer for every processors
     pcord_t * buffer_up = (pcord_t*)calloc(ysize, sizeof(pcord_t));
@@ -90,12 +90,34 @@ int main (int argc, char ** argv) {
     pcord_t * buffer_left = (pcord_t*)calloc(xsize, sizeof(pcord_t));
     pcord_t * buffer_right = (pcord_t*)calloc(xsize, sizeof(pcord_t));
     MPI_Status status;
-    int time_step=0;
+    int time_step=1, max_time=3600;//1 min of time_step 60*60
     int up,down,right,left;
     MPI_Cart_shift(grid_comm,0,1,&up,&down);
     MPI_Cart_shift(grid_comm,1,1,&left,&right);
-    for(time_step=0; time_step<=3600;time_step++){//1 min of time_step
-        //printf("P:%d My neighbors are r: %d d:%d 1:%d u:%d\n",rank,right,down,left,up);
+    //printf("P:%d My neighbors are r: %d d:%d 1:%d u:%d\n",rank,right,down,left,up);
+    for(time_step=1; time_step<=max_time;time_step++){
+        //check interation inside of the matrix. And calculate the buffer in the case of have neighbour
+        for (x=0; x<xsize; x++) {
+            for (y=0; y<ysize; y++) {
+                if(x==0 && left!=-1){
+                    buffer_left[y]=*GetMatrixValue(&m,x,y);
+                }else if(x==xsize-1 && right!=-1){
+                    buffer_right[y]=*GetMatrixValue(&m,x,y);
+                }else if(y==0 && up!=-1){
+                    buffer_up[x]=*GetMatrixValue(&m,x,y);
+                }else if(y==ysize-1 && down!=-1){
+                    buffer_down[x]=*GetMatrixValue(&m,x,y);
+                }else if(x>0 && y>0 && x<xsize-1 && y<ysize-1){
+                    if(collide(GetMatrixValue(&m,x,y), GetMatrixValue(&m,x,y+1))==1 &&
+                    collide(GetMatrixValue(&m,x,y), GetMatrixValue(&m,x+1,y))==1    &&
+                    collide(GetMatrixValue(&m,x,y), GetMatrixValue(&m,x,y-1))==1 &&
+                    collide(GetMatrixValue(&m,x,y), GetMatrixValue(&m,x-1,y))==1){ //dont move
+                        feuler(GetMatrixValue(&m,x,y),time_step) ;
+                    }
+                }
+            }
+        }
+        //check interation the the right colum, without neighbors
         if(right==-1){
             for (x=0; x<xsize; x++) {
                 if(x<xsize-1 && x>0){
@@ -116,7 +138,10 @@ int main (int argc, char ** argv) {
                     }
                 }
             }
+        }else{//if it has neighbour send buffer
+            MPI_Send(buffer_right,xsize,PART_MPI,right,RIGHTTAG,grid_comm);
         }
+        //check interation the the left colum, without neighbors
         if(left==-1){
             for (x=0; x<xsize; x++) {
                 if(x<xsize-1 && x>0){
@@ -137,7 +162,10 @@ int main (int argc, char ** argv) {
                     }
                 }
             }
+        }else{//if it has neighbour send buffer
+            MPI_Send(buffer_left,xsize,PART_MPI,left,LEFTTAG,grid_comm);
         }
+        //check interation the the up row, without neighbors
         if(up==-1){
             for (y=0; y<ysize; y++) {
                 if(y<ysize-1 && y>0){
@@ -158,8 +186,10 @@ int main (int argc, char ** argv) {
                     }
                 }
             }
+        }else{//if it has neighbour send buffer
+            MPI_Send(buffer_up,ysize,PART_MPI,up,UPTAG,grid_comm);
         }
-
+        //check interation the the down row, without neighbors
         if(down==-1){
             for (y=0; y<ysize; y++) {
                 if(y<ysize-1 && y>0){
@@ -180,57 +210,11 @@ int main (int argc, char ** argv) {
                     }
                 }
             }
+        }else{//if it has neighbour send buffer
+            MPI_Send(buffer_down,ysize,PART_MPI,down,DOWNTAG,grid_comm);
         }
-
-        for (x=0; x<xsize; x++) {
-            for (y=0; y<ysize; y++) {
-                    if(x==0 && left!=-1){
-                        buffer_left[y]=*GetMatrixValue(&m,x,y);
-                    }else if(x==xsize-1 && right!=-1){
-                        buffer_right[y]=*GetMatrixValue(&m,x,y);
-                    }else if(y==0 && up!=-1){
-                        buffer_up[x]=*GetMatrixValue(&m,x,y);
-                    }else if(y==ysize-1 && down!=-1){
-                        buffer_down[x]=*GetMatrixValue(&m,x,y);
-                    }else if(x>0 && y>0 && x<xsize-1 && y<ysize-1){
-                        if(collide(GetMatrixValue(&m,x,y), GetMatrixValue(&m,x,y+1))==1 &&
-                        collide(GetMatrixValue(&m,x,y), GetMatrixValue(&m,x+1,y))==1    &&
-                        collide(GetMatrixValue(&m,x,y), GetMatrixValue(&m,x,y-1))==1 &&
-                        collide(GetMatrixValue(&m,x,y), GetMatrixValue(&m,x-1,y))==1){ //dont move
-                            feuler(GetMatrixValue(&m,x,y),time_step) ;
-                        }
-                    }
-
-            }
-        }
-        if(right!=-1){
-            MPI_Send(buffer_right,xsize,PART_MPI,right,RIGHTTAG,grid_comm);
-            MPI_Recv(buffer_right,xsize,PART_MPI,right,LEFTTAG,grid_comm,&status);
-            for (x=0; x<xsize; x++) {
-                if(x<xsize-1 && x>0){
-                    if(collide(GetMatrixValue(&m,x,ysize-1), GetMatrixValue(&m,x,ysize-2))==1 &&
-                    collide(GetMatrixValue(&m,x,ysize-1), GetMatrixValue(&m,x+1,ysize-1))==1  &&
-                    collide(GetMatrixValue(&m,x,ysize-1), GetMatrixValue(&m,x-1,ysize-1))==1 &&
-                    collide(GetMatrixValue(&m,x,ysize-1), &buffer_right[x])==1){ //dont move
-                        feuler(GetMatrixValue(&m,x,ysize-1),time_step) ;
-                    }
-                }else if(x==xsize-1){
-                    if(collide(GetMatrixValue(&m,x,ysize-1), GetMatrixValue(&m,x,ysize-2))==1 &&
-                    collide(GetMatrixValue(&m,x,ysize-1), GetMatrixValue(&m,x-1,ysize-1))==1 &&
-                    collide(GetMatrixValue(&m,x,ysize-1), &buffer_right[x])==1){ //dont move
-                        feuler(GetMatrixValue(&m,x,ysize-1),time_step) ;
-                    }
-                }else if(x==0){
-                    if(collide(GetMatrixValue(&m,x,ysize-1), GetMatrixValue(&m,x,ysize-2))==1 &&
-                    collide(GetMatrixValue(&m,x,ysize-1), GetMatrixValue(&m,x+1,ysize-1))==1 &&
-                    collide(GetMatrixValue(&m,x,ysize-1), &buffer_right[x])==1){ //dont move
-                        feuler(GetMatrixValue(&m,x,ysize-1),time_step) ;
-                    }
-                }
-            }
-        }
+        //check interation the the left colum, with neighbors
         if(left!=-1){
-            MPI_Send(buffer_left,xsize,PART_MPI,left,LEFTTAG,grid_comm);
             MPI_Recv(buffer_left,xsize,PART_MPI,left,RIGHTTAG,grid_comm,&status);
             for (x=0; x<xsize; x++) {
                 if(x<xsize-1 && x>0){
@@ -255,34 +239,34 @@ int main (int argc, char ** argv) {
                 }
             }
         }
-        if(up!=-1){
-            MPI_Send(buffer_up,ysize,PART_MPI,up,UPTAG,grid_comm);
-            MPI_Recv(buffer_up,ysize,PART_MPI,up,DOWNTAG,grid_comm,&status);
-            for (y=0; y<ysize; y++) {
-                if(y<ysize-1 && y>0){
-                    if(collide(GetMatrixValue(&m,0,y), GetMatrixValue(&m,1,y))==1 &&
-                    collide(GetMatrixValue(&m,0,y), GetMatrixValue(&m,0,y+1))==1  &&
-                    collide(GetMatrixValue(&m,0,y), GetMatrixValue(&m,0,y-1))==1 &&
-                    collide(GetMatrixValue(&m,0,y), &buffer_up[y])==1){ //dont move
-                        feuler(GetMatrixValue(&m,0,y),time_step) ;
+        //check interation the the right colum, with neighbors
+        if(right!=-1){
+            MPI_Recv(buffer_right,xsize,PART_MPI,right,LEFTTAG,grid_comm,&status);
+            for (x=0; x<xsize; x++) {
+                if(x<xsize-1 && x>0){
+                    if(collide(GetMatrixValue(&m,x,ysize-1), GetMatrixValue(&m,x,ysize-2))==1 &&
+                    collide(GetMatrixValue(&m,x,ysize-1), GetMatrixValue(&m,x+1,ysize-1))==1  &&
+                    collide(GetMatrixValue(&m,x,ysize-1), GetMatrixValue(&m,x-1,ysize-1))==1 &&
+                    collide(GetMatrixValue(&m,x,ysize-1), &buffer_right[x])==1){ //dont move
+                        feuler(GetMatrixValue(&m,x,ysize-1),time_step) ;
                     }
-                }else if(y==ysize-1){
-                    if(collide(GetMatrixValue(&m,0,y), GetMatrixValue(&m,1,y))==1 &&
-                    collide(GetMatrixValue(&m,0,y), GetMatrixValue(&m,0,y-1))==1 &&
-                    collide(GetMatrixValue(&m,0,y), &buffer_up[y])==1){ //dont move
-                        feuler(GetMatrixValue(&m,0,y),time_step) ;
+                }else if(x==xsize-1){
+                    if(collide(GetMatrixValue(&m,x,ysize-1), GetMatrixValue(&m,x,ysize-2))==1 &&
+                    collide(GetMatrixValue(&m,x,ysize-1), GetMatrixValue(&m,x-1,ysize-1))==1 &&
+                    collide(GetMatrixValue(&m,x,ysize-1), &buffer_right[x])==1){ //dont move
+                        feuler(GetMatrixValue(&m,x,ysize-1),time_step) ;
                     }
-                }else if(y==0){
-                    if(collide(GetMatrixValue(&m,0,y), GetMatrixValue(&m,1,y))==1 &&
-                    collide(GetMatrixValue(&m,0,y), GetMatrixValue(&m,0,y+1))==1 &&
-                    collide(GetMatrixValue(&m,0,y), &buffer_up[y])==1){ //dont move
-                        feuler(GetMatrixValue(&m,0,y),time_step) ;
+                }else if(x==0){
+                    if(collide(GetMatrixValue(&m,x,ysize-1), GetMatrixValue(&m,x,ysize-2))==1 &&
+                    collide(GetMatrixValue(&m,x,ysize-1), GetMatrixValue(&m,x+1,ysize-1))==1 &&
+                    collide(GetMatrixValue(&m,x,ysize-1), &buffer_right[x])==1){ //dont move
+                        feuler(GetMatrixValue(&m,x,ysize-1),time_step) ;
                     }
                 }
             }
         }
+        //check interation the the down row, with neighbors
         if(down!=-1){
-            MPI_Send(buffer_down,ysize,PART_MPI,down,DOWNTAG,grid_comm);
             MPI_Recv(buffer_down,ysize,PART_MPI,down,UPTAG,grid_comm,&status);
             for (y=0; y<ysize; y++) {
                 if(y<ysize-1 && y>0){
@@ -307,31 +291,58 @@ int main (int argc, char ** argv) {
                 }
             }
         }
-        
+        //check interation the the up row, with neighbors
+        if(up!=-1){
+            MPI_Recv(buffer_up,ysize,PART_MPI,up,DOWNTAG,grid_comm,&status);
+            for (y=0; y<ysize; y++) {
+                if(y<ysize-1 && y>0){
+                    if(collide(GetMatrixValue(&m,0,y), GetMatrixValue(&m,1,y))==1 &&
+                    collide(GetMatrixValue(&m,0,y), GetMatrixValue(&m,0,y+1))==1  &&
+                    collide(GetMatrixValue(&m,0,y), GetMatrixValue(&m,0,y-1))==1 &&
+                    collide(GetMatrixValue(&m,0,y), &buffer_up[y])==1){ //dont move
+                        feuler(GetMatrixValue(&m,0,y),time_step) ;
+                    }
+                }else if(y==ysize-1){
+                    if(collide(GetMatrixValue(&m,0,y), GetMatrixValue(&m,1,y))==1 &&
+                    collide(GetMatrixValue(&m,0,y), GetMatrixValue(&m,0,y-1))==1 &&
+                    collide(GetMatrixValue(&m,0,y), &buffer_up[y])==1){ //dont move
+                        feuler(GetMatrixValue(&m,0,y),time_step) ;
+                    }
+                }else if(y==0){
+                    if(collide(GetMatrixValue(&m,0,y), GetMatrixValue(&m,1,y))==1 &&
+                    collide(GetMatrixValue(&m,0,y), GetMatrixValue(&m,0,y+1))==1 &&
+                    collide(GetMatrixValue(&m,0,y), &buffer_up[y])==1){ //dont move
+                        feuler(GetMatrixValue(&m,0,y),time_step) ;
+                    }
+                }
+            }
+        }
+
+
     }
     printf("ID= %d finish check collision \n",rank );
-
-
-
-
-
+    //TODO barrier
 /*
-    float totalmomentum=0;
+    float momentum=0;
+    //define wall
     struct cord wall;
     wall.x0=0;
     wall.y0=0;
-    wall.x1=xsize;
-    wall.y1=ysize;
+    wall.x1=xsize-1;
+    wall.y1=ysize-1;
     check with all the particles
     for (x=0; x<xsize; x++) {
         for (y=0; y<ysize; y++) {
-            totalmomentum+=wall_collide(GetMatrixValue(&m,x,y),wall);
+            momentum+=wall_collide(GetMatrixValue(&m,x,y),wall);
         }
     }
     //printf("ID= %d finish calculate totalmomentum=%lf\n",rank,totalmomentum );
+    MPIReduce( momentum, momentum, 1,MPI_FLOAT, SUM, 0,grid_comm );
     //reduction to id =0 and printf only id==0
-
-    printf("Total pressure =%lf\n", totalmomentum/(time_step*WALL_LENGTH));
+    if(rank==0){
+        printf("finish calculate totalmomentum=%lf\n",momentum );
+        printf("Total pressure =%lf\n", momentum/(time_step*WALL_LENGTH));
+    }
 */
     freeMatrix(&m);
     MPI_Finalize();
